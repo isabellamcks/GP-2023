7fdee0a28da74d046a8f1060ed33485a33e5020d
isabellamcks <145370220+isabellamcks@users.noreply.github.com>
2024-03-21 12:18:16 +0000

    ok

---
+++ Game Design Examples/ColorRect.gd
@@ -0,0 +1,27 @@
+extends ColorRect
+
+var speed = 100
+var rot_speed = 180
+
+
+
+func _ready():
+	pass
+	
+func _process(delta):
+	position.x += speed * delta 
+	position.y 
+	var r get_viewport_rect()
+	print(r)
+	if position.x > r.size.x:
+		speed = - speed
+	if position.y > r.size.y:
+		speedX = -100
+	if position.x < 0:
+		speedX = 0
+		speedY = -100
+
+		# translate(Vector2.RIGHT)
+	# rotation += deg_to_rad(rot_speed) * delta
+	pass
+	
